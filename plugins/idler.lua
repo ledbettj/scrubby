27bce36a741261d1342ea5666a4cad07b136e4cb
John Ledbetter <john.ledbetter@gmail.com>
2024-04-12 19:38:15 -0400

    activity usage

---
+++ plugins/idler.lua
@@ -0,0 +1,27 @@
+local bot = require("bot")
+
+local plugin = bot.plugin("Idler")
+
+local last = 0
+local games = {
+   "HoboQuest II",
+   "SimToilet Tycoon",
+   "Microsoft GarbageTruck Simulator",
+   "Minesweeper 2: Mine's Revenge",
+   "Tom Clancy's The Janitorial"
+}
+function plugin:tick(ctx)
+   if os.time() < last + 60 * 5 then
+      return
+   end
+   local game = games[math.random(1, #games)]
+   ctx:set_activity(game)
+   last = os.time()
+end
+
+
+function plugin:ready(ctx)
+   ctx:idle()
+end
+
+bot:register(plugin)
