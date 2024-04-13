ef1f96feae1a24bb919b5615c0ec92f15a84667b
John Ledbetter <john.ledbetter@gmail.com>
2024-04-13 11:09:05 -0400

    fixup spotify package

--- pkgs/spotify.lua
+++ pkgs/spotify.lua
@@ -2,9 +2,7 @@ local http = require('http')
 local json = require('json')
 local b64 = require('base64')
 
-local spotify = {
-   expires_at = 0
-}
+local Client = {}
 
 local function url_encode(str)
    local str = string.gsub(str, "([^%w%.%- ])", function(c)
@@ -14,20 +12,22 @@ local function url_encode(str)
    return str
 end
 
-function spotify:init(client_id, client_secret, code)
-   self.client_id = client_id
-   self.client_secret = client_secret
+function Client:new(client_id, client_secret)
+   client = { expires_at = 0, client_id = client_id, client_secret = client_secret }
+   setmetatable(client, self)
+   self.__index = self
+   return client
 end
 
-function spotify:auth(code)
-   self.code = code
-   local resp = http.post("https://accounts.spotify.com/api/token",
-                          "grant_type=authorization_code&code=" .. self.code .. "&redirect_uri=https://weirdhorse.party/callback",
-                          {
-                             headers = {
-                                ["Content-Type"] = "application/x-www-form-urlencoded",
-                                ["Authorization"] = "Basic " .. b64.encode(self.client_id .. ":" .. self.client_secret)
-                             }
+function Client:auth(code)
+   local resp = http.post(
+      "https://accounts.spotify.com/api/token",
+      "grant_type=authorization_code&code=" .. code .. "&redirect_uri=https://weirdhorse.party/callback",
+      {
+         headers = {
+            ["Content-Type"] = "application/x-www-form-urlencoded",
+            ["Authorization"] = "Basic " .. b64.encode(self.client_id .. ":" .. self.client_secret)
+         }
    })
    local data = json.decode(resp)
 
@@ -36,33 +36,39 @@ function spotify:auth(code)
    self.expires_at = os.time() + data.expires_in
 end
 
-function spotify:auth_refresh()
-   local resp = http.post("https://accounts.spotify.com/api/token",
-                          "grant_type=refresh_token&refresh_token=" .. self.refresh_token,
-                          {
-                             headers = {
-                                ["Content-Type"] = "application/x-www-form-urlencoded",
-                                ["Authorization"] = "Basic " .. b64.encode(self.client_id .. ":" .. self.client_secret)
-                             }
+function Client:auth_refresh()
+   local resp = http.post(
+      "https://accounts.spotify.com/api/token",
+      "grant_type=refresh_token&refresh_token=" .. self.refresh_token,
+      {
+         headers = {
+            ["Content-Type"] = "application/x-www-form-urlencoded",
+            ["Authorization"] = "Basic " .. b64.encode(self.client_id .. ":" .. self.client_secret)
+         }
    })
    local data = json.decode(resp)
+
    self.access_token = data.access_token
    self.refresh_token = data.refresh_token
    self.expires_at = os.time() + data.expires_in
 end
 
-function spotify:search(query)
-   local resp = http.get("https://api.spotify.com/v1/search?type=track&q=" .. url_encode(query) .. "&limit=1",
-                         { headers = { ['Authorization'] = "Bearer " .. self.access_token } })
+function Client:search(query)
+   local resp = http.get(
+      "https://api.spotify.com/v1/search?type=track&q=" .. url_encode(query) .. "&limit=1",
+      { headers = { ['Authorization'] = "Bearer " .. self.access_token } }
+   )
    local data = json.decode(resp)
 
    return data.tracks.items[1]
 end
 
-function spotify:enqueue(track_id)
-   http.post("https://api.spotify.com/v1/me/player/queue?uri=" .. url_encode(track_id),
-             "",
-             { headers = { ['Authorization'] = "Bearer " .. self.access_token } })
+function Client:enqueue(track_id)
+   http.post(
+      "https://api.spotify.com/v1/me/player/queue?uri=" .. url_encode(track_id),
+      "",
+      { headers = { ['Authorization'] = "Bearer " .. self.access_token } }
+   )
 end
 
-return spotify
+return { Client = Client }
--- plugins/spotify.lua
+++ plugins/spotify.lua
@@ -3,41 +3,40 @@ local spotify = require("spotify")
 
 local plugin = bot.plugin("Spotify")
 
--- spotify:init(
---    "c021ca2ee0c943e1835fdbef8b89b1cd",
---    "CLIENT_SECRET"
--- )
-
+local client = spotify.Client:new(
+   "c021ca2ee0c943e1835fdbef8b89b1cd",
+   "--SECRET--"
+)
 
 plugin.cache:load()
-
 
 local refresh_token = plugin.cache:get("refresh_token")
 local access_token = plugin.cache:get("access_token")
 local expires_at = plugin.cache:get("expires_at")
 
 if access_token ~= nil and expires_at > os.time() then
-   spotify.refresh_token = refresh_token
-   spotify.access_token = access_token
-   spotify.expires_at = expires_at
+   client.refresh_token = refresh_token
+   client.access_token = access_token
+   client.expires_at = expires_at
 else
    print("https://accounts.spotify.com/authorize?response_type=code&client_id=c021ca2ee0c943e1835fdbef8b89b1cd&scope=user-read-private+user-read-email+user-modify-playback-state&redirect_uri=https://weirdhorse.party/callback")
    -- get code
    -- put it below
    local code = io.read()
-   spotify:auth(code)
-   plugin.cache:set("refresh_token", spotify.refresh_token)
-   plugin.cache:set("access_token", spotify.access_token)
-   plugin.cache:set("expires_at", spotify.expires_at)
+   client:auth(code)
+
+   plugin.cache:set("refresh_token", client.refresh_token)
+   plugin.cache:set("access_token", client.access_token)
+   plugin.cache:set("expires_at", client.expires_at)
    plugin.cache:save()
 end
 
 function plugin:tick(ctx)
-   if os.time() > spotify.expires_at - 60 * 5 then
-      spotify:auth_refresh()
-      plugin.cache:set("refresh_token", spotify.refresh_token)
-      plugin.cache:set("access_token", spotify.access_token)
-      plugin.cache:set("expires_at", spotify.expires_at)
+   if os.time() > client.expires_at - 60 * 5 then
+      client:auth_refresh()
+      plugin.cache:set("refresh_token", client.refresh_token)
+      plugin.cache:set("access_token", client.access_token)
+      plugin.cache:set("expires_at", client.expires_at)
       print("Refreshed spotify token")
       plugin.cache:save()
    end
@@ -45,8 +44,8 @@ end
 
 plugin:command(".*queue up\\s+(.*)", function(msg, matches)
   local query = matches[2]
-  local r = spotify:search(query)
-  spotify:enqueue(r.uri)
+  local r = client:search(query)
+  client:enqueue(r.uri)
 
   return {
      embed = {
