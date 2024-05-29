b332987e19e84896fd544b5a742fb940d95a5447
John Ledbetter <john.ledbetter@gmail.com>
2024-05-29 19:37:41 -0400

    tmp

--- plugins/cat_fact.lua
+++ plugins/cat_fact.lua
@@ -4,7 +4,7 @@ local json = require("json")
 
 local plugin = bot.plugin("CatFact", "gives you facts. about cats.")
 
-plugin:command("\\bcat fact", "gives you a fact.  about cats.", function(self, msg, _)
+plugin:command("CatFact", "gives you a fact.  about cats.", function(self, msg, _)
   local fact = json.decode(http.get("https://catfact.ninja/fact").body)
   return fact["fact"]
 end)
